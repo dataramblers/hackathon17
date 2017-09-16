6f8523140734543a4e422d09c84d3f19117263d3
Sebastian <sebastian.schuepbach@unibas.ch>
2017-09-16 10:05:47 +0200

    Read file directory instead of single files

--- crossref/load-data-crossref.py
+++ crossref/load-data-crossref.py
@@ -3,14 +3,13 @@ from sys import argv
 import json
 import gzip
 import pyisbn
-
+import os
 
 # def isbn10toisbn13(isbn10):
 #     isbn13 = '978' + isbn10.replace('-', '')[0:9]
 #     s = sum([(int(isbn13[i]) - 48) * 1 if i % 2 == 0 else 3 for i in range(0, len(isbn13))])
 #     return isbn13 + str(10 - (s % 10))
 
-# es = Elasticsearch(http_auth=('elastic', 'changeme'))
 index = 'crossref'
 doctype = 'crossref'
 es = Elasticsearch()
@@ -18,22 +17,25 @@ cache = list()
 counter = 0
 bulksize = 10000
 
-with gzip.open(argv[1], 'rb') as f:
-    for l in f:
-        bigjsonobject = json.loads(l)
-        for jsonobj in bigjsonobject:
-            id = jsonobj['DOI']
-            if 'ISBN' in jsonobj.keys():
-                isbnlist = list()
-                for isbn in jsonobj['ISBN']:
-                    if len(isbn.replace('-','')) == 10:
-                        isbnlist.append(pyisbn.convert(isbn))
-                jsonobj['ISBN'] = isbnlist
-            header = '{ "index" : { "_index" : "' + index + '", "_type" : "' + doctype + '", "_id" : "' + str(hash(id)) + '" } }'
-            cache.append(header)
-            cache.append(json.dumps(jsonobj))
-            counter = counter + 1
-            if counter >= bulksize:
-                es.bulk(index=index, doc_type=doctype, body='\n'.join(cache))
-                cache = []
-                counter = 0
+for dirname, dirnames, filenames in os.walk(argv[1]):
+    for filename in filenames:
+        if filename.endswith('json.gz'):
+            with gzip.open(os.path.join(argv[1],filename), 'rb') as f:
+                for l in f:
+                    bigjsonobject = json.loads(l)
+                    for jsonobj in bigjsonobject:
+                        id = jsonobj['DOI']
+                        if 'ISBN' in jsonobj.keys():
+                            isbnlist = list()
+                            for isbn in jsonobj['ISBN']:
+                                if len(isbn.replace('-','')) == 10:
+                                    isbnlist.append(pyisbn.convert(isbn))
+                            jsonobj['ISBN'] = isbnlist
+                        header = '{ "index" : { "_index" : "' + index + '", "_type" : "' + doctype + '", "_id" : "' + str(hash(id)) + '" } }'
+                        cache.append(header)
+                        cache.append(json.dumps(jsonobj))
+                        counter = counter + 1
+                        if counter >= bulksize:
+                            es.bulk(index=index, doc_type=doctype, body='\n'.join(cache))
+                        cache = []
+                        counter = 0
