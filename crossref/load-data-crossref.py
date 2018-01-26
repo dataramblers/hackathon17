fbf87f04b9e38eb9b4a49ee06db82f729dba9afe
Kordishal <jonaswaeber@gmail.com>
2018-01-26 13:19:22 +0100

    updated mapping added function to remove unused fields. added error logging to upload

--- crossref/load-data-crossref.py
+++ crossref/load-data-crossref.py
@@ -4,11 +4,10 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 import json
 import lzma
-import gzip
 import pyisbn
 import os
-import time
 import argparse
+import time
 from tqdm import tqdm
 
 
@@ -19,21 +18,23 @@ class Importer:
     """
 
     def __init__(self, es_host="localhost", es_port=8080):
-        self.es = Elasticsearch([es_host + ':' + str(es_port) + '/'])
+        self.es = Elasticsearch([es_host + ':' + str(es_port) + '/'], timeout=3000)
+        self.deleted_key = list()
 
-    def batch(self, index, doc_type, body, counter=0):
+    def batch(self, body):
         """
         Load data into the index
-        :param index: Name of the index
-        :param doc_type: Name of the doc_type
         :param body: Data to index
-        :param counter: internal counter for failure retry
         :return:
         """
         errors = bulk(self.es, actions=body, raise_on_error=False)
+        if errors[0] < 10000:
+            with open('./logging/logs-' + str(time.time()) + '.json', 'w') as file:
+                file.write(json.dumps(errors[1], ensure_ascii=False, indent='    '))
+
         print(errors)
 
-    def bootstrap(self, index, doc_type, mapping_file="mapping.json"):
+    def bootstrap(self, index, mapping_file="mapping.json"):
         """
         Bootstrap es index. Delete existing index and create a new one with mapping
         :param index: Name of the index
@@ -47,7 +48,8 @@ class Importer:
         self.es.indices.create(index)
         self.es.indices.put_template('crossref', mapping)
 
-    def remove_affiliation(self, json_object, parent_json_obj):
+    @staticmethod
+    def remove_affiliation(json_object, parent_json_obj):
         """
         Removes `affiliation` field in embedded person object
         :param json_object: The current json object
@@ -57,6 +59,19 @@ class Importer:
         if parent_json_obj in json_object.keys():
             for t in json_object[parent_json_obj]:
                 t.pop('affiliation', None)
+        return json_object
+
+    def remove_unused_fields(self, json_object: dict):
+        used_fields = ['ISBN', 'ISSN', 'title', 'author', 'editor', 'subtitle', 'DOI', 'funder']
+        removal_keys = list()
+        for key in json_object:
+            if not used_fields.__contains__(key):
+                removal_keys.append(key)
+
+        for key in removal_keys:
+            if not self.deleted_key.__contains__(key):
+                self.deleted_key.append(key)
+            del json_object[key]
         return json_object
 
     def load(self, directory, index="crossref", doc_type="crossref", bulk_size=100000):
@@ -79,9 +94,7 @@ class Importer:
                         pbar_o.update()
                         with lzma.open(os.path.join(root, filename), 'rt', encoding='utf-8') as f:
                             line = f.readline()
-                            #with tqdm(total=len(lines)) as pbar:
                             while line:
-                                #pbar.update()
                                 json_object = json.loads(line)
                                 json_object['oid'] = json_object['_id']['$oid']
                                 del json_object['_id']
@@ -95,6 +108,7 @@ class Importer:
                                     json_object['ISBN'] = isbn_list
                                 json_object = self.remove_affiliation(json_object, 'author')
                                 json_object = self.remove_affiliation(json_object, 'editor')
+                                json_object = self.remove_unused_fields(json_object)
                                 data = dict()
                                 data['_op_type'] = 'index'
                                 data['_index'] = index
@@ -104,17 +118,20 @@ class Importer:
                                 cache.append(data)
                                 counter += 1
                                 if counter >= bulk_size:
-                                    self.batch(index, doc_type, cache)
+                                    self.batch(cache)
                                     cache = []
                                     counter = 0
                                 line = f.readline()
-                            #pbar.close()
+                        # after each file write down what keys were deleted.
+                        with open('logging/deleted_keys.txt', 'w', encoding='utf-8') as file:
+                            for key in self.deleted_key:
+                                file.write(key + '\n')
         pbar_o.close()
 
 
 parser = argparse.ArgumentParser(description='Load json data fro crossref into an elastic search index.')
 parser.add_argument('data', type=str, help='Path to the gziped data')
-parser.add_argument('--es-host', type=str, help='Elastic search uri', default="localhost")
+parser.add_argument('--es-host', type=str, help='Elastic search uri', default="sb-ues2.swissbib.unibas.ch")
 parser.add_argument('--es-port', type=int, help='Elastic search uri', default=8080)
 parser.add_argument('--es-index',  type=str, help='Elastic search index', default="crossref")
 parser.add_argument('--es-doc-type',  type=str, help='Elastic search doctype', default="crossref")
@@ -125,7 +142,7 @@ args = parser.parse_args()
 importer = Importer(args.es_host, args.es_port)
 # flush data and create index
 if args.es_flush:
-    importer.bootstrap(args.es_index, args.es_doc_type, args.mapping_file)
+    importer.bootstrap(args.es_index, args.mapping_file)
 # import data
 print(importer.es.cat.health())
 importer.load(args.data, args.es_index, args.es_doc_type, args.bulk_size)
